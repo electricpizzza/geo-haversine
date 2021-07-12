0cae4c98d7727bec226c58d4419c3306deb0eb3c
unknown <zackdinar1234@gmaail.com>
2021-07-12 10:43:43 +0100

    Updating the Haversine Formula

--- src/GeoHaversine.ts
+++ src/GeoHaversine.ts
@@ -28,40 +28,109 @@ export class GeoHaversine {
     const secondPointX = secondPoint.lat;
     const secondPointY = secondPoint.long;
 
-    return Math.sqrt(
-      Math.pow(firstPointX - secondPointX, 2) +
-        Math.pow(firstPointY - secondPointY, 2)
-    );
+    const R = 6371e3; // metres
+    const φ1 = (firstPointX * Math.PI) / 180; // φ, λ in radians
+    const φ2 = (secondPointX * Math.PI) / 180;
+    const Δφ = ((secondPointX - firstPointX) * Math.PI) / 180;
+    const Δλ = ((secondPointY - firstPointY) * Math.PI) / 180;
+    const a =
+      Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+      Math.cos(φ1) * Math.cos(φ2) +
+      Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    return R * c;
+  }
+
+  /**
+   * getDisttance
+   * is the same as getVectorieLength but with pont as params
+   * to Get distance between tow point on a map by gelo location long,lat
+   *
+   */
+  public getDisttance(firstPoint: number[], secondPoint: number[]) {
+    if (firstPoint.length != 2 || secondPoint.length != 2)
+      throw new Error("Position must be an array of tow numbers [Long,Lat]");
+
+    const firstPos = new Position(firstPoint);
+    const secondPos = new Position(secondPoint);
+    const firstPointX = firstPos.lat;
+    const firstPointY = firstPos.long;
+    const secondPointX = secondPos.lat;
+    const secondPointY = secondPos.long;
+
+    const R = 6371e3; // metres
+    const φ1 = (firstPointX * Math.PI) / 180; // φ, λ in radians
+    const φ2 = (secondPointX * Math.PI) / 180;
+    const Δφ = ((secondPointX - firstPointX) * Math.PI) / 180;
+    const Δλ = ((secondPointY - firstPointY) * Math.PI) / 180;
+    const a =
+      Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+      Math.cos(φ1) * Math.cos(φ2) +
+      Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    return R * c;
   }
 
   public getTheNearestOfTowPoints(
-    root: Position,
-    firstPoint: Position,
-    secondPoint: Position
+    root: number[],
+    firstPoint: number[],
+    secondPoint: number[]
   ): Position {
-    return this.getVectorialLength(root, firstPoint) >
-      this.getVectorialLength(root, secondPoint)
-      ? firstPoint
-      : secondPoint;
+    if (root.length != 2 || firstPoint.length != 2 || secondPoint.length != 2)
+      throw new Error("Position must be an array of tow numbers [Long,Lat]");
+
+    const posRoot = new Position(root);
+    const firstPos = new Position(firstPoint);
+    const secondPos = new Position(secondPoint);
+    return this.getVectorialLength(posRoot, firstPos) >
+      this.getVectorialLength(posRoot, secondPos)
+      ? firstPos
+      : secondPos;
   }
 
-  public getTheNearestPoint(root: Position, ...points: Position[]): Position {
-    let theNearest = points[0];
+  public getTheNearestPoint(root: number[], ...points: number[][]): Position {
+    if (root.length != 2)
+      throw new Error(
+        "Root Position must be an array of tow numbers [Long,Lat]"
+      );
+
+    const rootPos = new Position(root);
+    let theNearest = new Position(points[0]);
     points.forEach((point) => {
-      theNearest = this.getTheNearestOfTowPoints(root, point, theNearest);
+      if (point.length != 2)
+        throw new Error("Position must be an array of tow numbers [Long,Lat]");
+
+      theNearest = rootPos.getTheNearestOfTowPoints(
+        new Position(point),
+        theNearest
+      );
     });
     return theNearest;
   }
 
+  /**
+   * gettheNearestPointsByRangeAlt
+   * this methode is an alternative
+   */
   public gettheNearestPointsByRange(
-    root: Position,
+    root: number[],
     range: number,
-    ...points: Position[]
+    ...points: number[][]
   ): Position[] {
+    if (root.length != 2)
+      throw new Error("Position must be an array of tow numbers [Long,Lat]");
+
+    const rootPos = new Position(root);
     const theNearestPositions: Position[] = [];
+
     points.forEach((point) => {
-      if (this.getVectorialLength(root, point) < range) {
-        theNearestPositions.push(point);
+      if (point.length != 2)
+        throw new Error("Position must be an array of tow numbers [Long,Lat]");
+
+      if (this.getVectorialLength(rootPos, new Position(point)) < range) {
+        theNearestPositions.push(new Position(point));
       }
     });
     return theNearestPositions;
--- src/Range.ts
+++ src/Range.ts
@@ -2,3 +2,32 @@ export class Range {
   constructor(public distance: number) {}
   private earthR = 12742 / 2;
 }
+
+/**
+ *
+ * **********************************
+ * ****** Haversine Formyula*********
+ * **********************************
+ *
+ * Haversine
+ * formula:	a = sin²(Δφ/2) + cos φ1 ⋅ cos φ2 ⋅ sin²(Δλ/2)
+ * c = 2 ⋅ atan2( √a, √(1−a) )
+ * d = R ⋅ c
+ *
+ * **********************************
+ * ************* The Code ***********
+ * **********************************
+ *
+ * const R = 6371e3; // metres
+ * const φ1 = lat1 * Math.PI/180; // φ, λ in radians
+ * const φ2 = lat2 * Math.PI/180;
+ * const Δφ = (lat2-lat1) * Math.PI/180;
+ * const Δλ = (lon2-lon1) * Math.PI/180;  *
+ * const a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+ *           Math.cos(φ1) * Math.cos(φ2)
+ *           Math.sin(Δλ/2) * Math.sin(Δλ/2);
+ *
+ * const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+ *
+ * const d = R * c; // in metres
+ */
